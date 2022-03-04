a316511da8409364ea5dd1cec5dc2ec380e57f32
vincent vitale <vincent.vitale87@gmail.com>
2022-03-04 11:38:19 -0800

    fixing redis client issues

--- queues.js
+++ queues.js
@@ -2,34 +2,30 @@ const Queue = require("bull");
 const { match: matchWorker } = require("./workers");
 
 if (process.env.REDISTOGO_URL) {
-  console.log(`REDIS TO GO URL ~~~ ${process.env.REDISTOGO_URL}`);
   const rtg = require("url").parse(process.env.REDISTOGO_URL);
-  console.log(`REDIS TO GO RTG ~~~ ${rtg}`);
-  console.log(`REDIS TO GO PORT ~~~ ${rtg.port}`);
-  console.log(`REDIS TO GO HOST NAME ~~~ ${rtg.hostname}`);
   var redis = require("redis").createClient(rtg.port, rtg.hostname);
-
   redis.auth(rtg.auth.split(":")[1]);
+
   const match = new Queue("match", {
     redis,
   });
-  redis.connect()
+
+  console.log(`NEW QUE COMPLETE`);
 
   match.process((job, done) => {
     matchWorker(job, done);
   });
 
+  console.log(`MATCH PROCESS COMPLETE`);
+
   const queues = [
     {
       name: "match",
       hostId: "Match Que Managers",
-      redis: {
-        port: process.env.REDIS_PORT || 6379,
-        host: "127.0.0.1",
-        db: 1,
-      },
+      redis,
     },
   ];
+  console.log(`QUEUES ~~~ ${queues}`);
 
   module.exports = { match, queues };
 } else {
