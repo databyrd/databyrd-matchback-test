d2e75db26ab78d6e35a07db1927429966c722f39
vincent vitale <vincent.vitale87@gmail.com>
2022-03-04 12:10:00 -0800

    adding redis connect

--- queues.js
+++ queues.js
@@ -6,9 +6,16 @@ if (process.env.REDISTOGO_URL) {
   var redis = require("redis").createClient(rtg.port, rtg.hostname);
   redis.auth(rtg.auth.split(":")[1]);
 
+  redis.on('connect', function() {
+    console.log('Redis client connected');
+    console.log(`${client.connected}`);
+});
+
   redis.on("error", (err) => {
     console.log(`REDIS CONNECTION ERROR ~~~ ${err}`);
   });
+
+ 
 
   const match = new Queue("match", {
     redis,
