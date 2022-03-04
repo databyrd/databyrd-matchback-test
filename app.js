d6c856630baa5869a8cc0608c7e4e8188d736c12
vincent vitale <vincent.vitale87@gmail.com>
2022-03-04 15:54:18 -0800

    redis revert

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const timeout = require("connect-timeout");
 const Arena = require("bull-arena");
 const Bull = require("bull");
 const { queues } = require("./queues");
+const {redis} = require("./helpers/redis");
 require("dotenv").config();
 // require("./helpers/redis");
 
@@ -39,32 +40,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "client/build")));
 // --------------------------------
 
-// REDIS //
-const rtg = require("url").parse(process.env.REDISTOGO_URL);
-var redis = require("redis").createClient({
-  port: rtg.port,
-  host: rtg.hostname,
-});
-console.log(`REDIS AUTH PASSWORD ~~~ ${rtg.auth.split(":")[1]}`);
-redis.auth(rtg.auth.split(":")[1]);
-console.log(`REDIS AUTH`);
-redis.connect();
-console.log(`REDIS CONNECT`);
-redis.on("connect", function () {
-  console.log("Redis client connected");
-});
-console.log(`REDIS READY`);
-redis.on("ready", () => {
-  console.log("Redis ready to be used");
-});
-console.log(`REDIS ERROR`);
-redis.on("error", (err) => {
-  console.log(`REDIS CONNECTION ERROR ~~~ ${err}`);
-});
-
-redis.on("end", () => {
-  console.log("Client disconnected from Redis");
-});
 // --------THIS ENTIRE SECTION IS FOR LARGE FILE UPLOADS ----------- //
 
 const arenaConfig = Arena(
