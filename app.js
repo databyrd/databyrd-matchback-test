96635e7e773426dce4cb7c69fce368c05d082ff1
vincent vitale <vincent.vitale87@gmail.com>
2022-03-06 23:53:02 -0800

    cleaning up

--- app.js
+++ app.js
@@ -39,52 +39,26 @@ app.use(express.static(path.join(__dirname, "client/build")));
 // --------------------------------
 
 // --------THIS ENTIRE SECTION IS FOR LARGE FILE UPLOADS ----------- //
-if (process.env.REDISTOGO_URL) {
-  console.log(`REDIS TO GO URL FOUND ~~~~ ${process.env.REDISTOGO_URL}`);
-  try {
-    redis.connect();
-    redis.on("connect", function () {
-      console.log("Redis client connected");
-    }),
-      redis.on("ready", () => {
-        console.log("Redis ready to be used");
-      }),
-      redis.on("error", (err) => {
-        console.log(`REDIS CONNECTION ERROR ~~~ ${err}`);
-      }),
-      redis.on("end", () => {
-        console.log("Client disconnected from Redis");
-      });
 
-    console.log(`REDIS ~~~ ${redis}`);
-
-    const arenaConfig = Arena(
+const arenaConfig = Arena(
+  {
+    Bull,
+    queues: [
       {
-        Bull,
-        queues: [
-          {
-            name: "match",
-            hostId: "Match Que Managers",
-            redis,
-          },
-        ],
+        name: "match",
+        hostId: "Match Que Managers",
+        redis,
       },
-      {
-        basePath: "/arena",
-        disableListen: true,
-      }
-    );
-    console.log(`ARENAS ~~~ ${arenaConfig}`);
-    app.use("/", arenaConfig);
-    console.log(`EXITING REDIS TO GO `);
-  } catch (error) {
-    console.log(error);
+    ],
+  },
+  {
+    basePath: "/arena",
+    disableListen: true,
   }
-  
-}
+);
 
+app.use("/", arenaConfig);
 app.use("/", indexRouter);
-
 app.use("/users", usersRouter);
 // --------END LARGE FILE UPLOAD SECTION ----------- //
 
