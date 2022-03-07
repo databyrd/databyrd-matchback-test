7b08b526dbb3dcd7afa793280e0fab5d98940dd9
vincent vitale <vincent.vitale87@gmail.com>
2022-03-07 00:56:41 -0800

    removing queues

--- queues.js
+++ queues.js
@@ -1,52 +1,52 @@
-const Queue = require("bull");
-const { match: matchWorker } = require("./workers/index");
-// const redisClient = require("./helpers/redis");
-if (process.env.REDISTOGO_URL) {
-  const match = new Queue("match", {
-    redisClient,
-  });
-  console.log(`REDIS CONNECT ~~~ ${redisClient.isOpen} QUEUES.JS`);
-  match.process((job, done) => {
-    matchWorker(job, done);
-  });
+// const Queue = require("bull");
+// const { match: matchWorker } = require("./workers/index");
+// // const redisClient = require("./helpers/redis");
+// if (process.env.REDISTOGO_URL) {
+//   const match = new Queue("match", {
+//     redisClient,
+//   });
+//   console.log(`REDIS CONNECT ~~~ ${redisClient.isOpen} QUEUES.JS`);
+//   match.process((job, done) => {
+//     matchWorker(job, done);
+//   });
 
 
 
-  const queues = [
-    {
-      name: "match",
-      hostId: "Match Que Managers",
-      redisClient,
-    },
-  ];
+//   const queues = [
+//     {
+//       name: "match",
+//       hostId: "Match Que Managers",
+//       redisClient,
+//     },
+//   ];
  
-  module.exports = { match, queues };
-} else {
-  redis = require("redis").createClient();
+//   module.exports = { match, queues };
+// } else {
+//   redis = require("redis").createClient();
 
-  const match = new Queue("match", {
-    redis,
-  });
+//   const match = new Queue("match", {
+//     redis,
+//   });
 
-  match.process((job, done) => {
-    matchWorker(job, done);
-  });
+//   match.process((job, done) => {
+//     matchWorker(job, done);
+//   });
 
-  const queues = [
-    {
-      name: "match",
-      hostId: "Match Que Managers",
-      redis: {
-        port: process.env.REDIS_PORT || 6379,
-        host: "127.0.0.1",
-        db: 1,
-      },
-    },
-  ];
-  console.log(`QUEUES FROM QUEUE PAGE ${queues}`)
+//   const queues = [
+//     {
+//       name: "match",
+//       hostId: "Match Que Managers",
+//       redis: {
+//         port: process.env.REDIS_PORT || 6379,
+//         host: "127.0.0.1",
+//         db: 1,
+//       },
+//     },
+//   ];
+//   console.log(`QUEUES FROM QUEUE PAGE ${queues}`)
 
-  module.exports = { match, queues };
-}
+//   module.exports = { match, queues };
+// }
 
 // const match = new Queue("match", {
 //   redis,
