b59b9d6130b859c6c0d5b410c098eeca266ad618
vincent vitale <vincent.vitale87@gmail.com>
2022-03-08 15:54:07 -0800

    adding logger

--- routes/index.js
+++ routes/index.js
@@ -95,7 +95,7 @@ router.post("/node-api/compare-large-files", async function (req, res, next) {
   const originalPath = req.body.originalId;
   const comparedPath = req.body.compareId;
   console.log(`COMPARE LARGE FILES ~~~ ${originalPath} ~~~ ${comparedPath}`);
-
+console.log(match)
   const jobData = await match.add({ originalPath, comparedPath });
   console.log(`JOB DATA ID NUMBER ~~~ ${jobData}`);
   console.log("COMPLETE", jobData.id);
